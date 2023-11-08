84823f7157b2477e6b788eb7851930badfa057d7
Eddy Wister <e@wister.mx>
2023-11-08 15:51:45 -0600

    New Class

---
+++ API-Minimizer/Model/Enviroments.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Minimizer.Model
+{
+    public class Enviroments
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string ClusterValue { get; set; }
+        public string AppSettingValue { get; set; }
+    }
+}
